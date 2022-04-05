1fe3d56616a16d5af41e21a21a9cd4148a78b182
the-kawatra <ravan.budh@gmail.com>
2022-04-05 17:51:02 +0530

    Input is a controlled component

--- src/Components/User/Search.js
+++ src/Components/User/Search.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
   render() {
     return (
-        <div>
-            <form>
-                <input type="text" placeholder='Search User'/>
-                <input type="submit" className='btn btn-dark btn-block'/>
-            </form>
-        </div>
-    )
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Search User"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <input type="submit" className="btn btn-dark btn-block" />
+        </form>
+      </div>
+    );
   }
 }
 
-export default Search
+export default Search;
